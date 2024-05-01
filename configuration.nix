babb7cdfbed7f1e2769217ccd54fdefb6bcb1326
Waffle Lapkin <waffle.lapkin@gmail.com>
2024-05-01 16:46:06 +0200

    Reorder stuff for readability

--- configuration.nix
+++ configuration.nix
@@ -16,88 +16,139 @@
       inputs.nixos-hardware.nixosModules.common-pc-laptop-acpi_call
       inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
     ];
-  boot.kernelParams = [
-    # disables the watchdogs so that I can actually turn off the laptop normally lol :|
-    "nowatchdog"
-    "modprobe.blacklist=sp5100_tco,iTCO_wdt,edac_mce_amd"
-    # disable psr (which causes amdgpu crashes) (0x18B & ~0x8)
-    "amdgpu.dcfeaturemask=0x183"
-  ];
-  hardware.sensor.iio.enable = true; # auto rotate screen?
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.allowUnfree = true; # we want drivers lol
   nixpkgs.overlays = [ inputs.fenix.overlays.default ];
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [
+      # disables the watchdogs so that I can actually turn off the laptop normally lol :|
+      "nowatchdog"
+      "modprobe.blacklist=sp5100_tco,iTCO_wdt,edac_mce_amd"
+      # disable psr (which causes amdgpu crashes) (0x18B & ~0x8)
+      "amdgpu.dcfeaturemask=0x183"
+    ];
+    initrd.luks.devices.nixos-enc = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+  };
+  fileSystems."/boot".options = [ "umask=0077" ];
+
+  networking.hostName = "polaris"; # Define your hostname.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  systemd.services.NetworkManager-wait-online.enable = false; # workaround for a bug <https://github.com/NixOS/nixpkgs/issues/180175>
+  time.timeZone = "Europe/Amsterdam";
+
   ## try to fix visual bugs :sob:
   #chaotic.mesa-git.enable = true;
   #chaotic.mesa-git.method = "replaceRuntimeDependencies";
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  networking.hostName = "polaris"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-  systemd.services.NetworkManager-wait-online.enable = false; # workaround for a bug <https://github.com/NixOS/nixpkgs/issues/180175>
+
+  hardware = {
+    enableRedistributableFirmware = true;
+    bluetooth.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        nvidiaBusId = "PCI:01:0:0";
+        amdgpuBusId = "PCI:69:0:0"; # nice
+      };
+    };
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+    sensor.iio.enable = true; # auto rotate screen?
+  };
+
+  # In theory this allows me to change asus weird stuff
+  # (in practice it doesn't)
   services.asusd = {
     enable = true;
     enableUserService = true;
   };
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  boot.initrd.luks.devices.nixos-enc = {
-    device = "/dev/nvme0n1p2";
-    preLVM = true;
-  };
-  services.udev.extraHwdb = ''
-    # Rebinds keys on elecom huge trackball
-    #
-    # tip: You can use `sudo evtest` to get the key values and stuff.
-    #
-    # 90001 = L
-    # 90002 = R
-    # 90003 = M (scroll wheel button)
-    # 90004 = < (back)
-    # 90005 = > (forward)
-    # 90006 = Fn1
-    # 90007 = Fn2
-    # 90008 = Fn3
-    #
-    # key_blue -- noop
-    # btn_0 -- corresponds to key code 256. idk what it does,
-    #          but we can make kwin see it as "scroll" button
-    #          (sadly kwin only wants btn_*, key_* won't work...)
-    evdev:input:b0003v056Ep010D*
-      KEYBOARD_KEY_90001=key_blue
-      KEYBOARD_KEY_90002=btn_0
-      KEYBOARD_KEY_90003=key_blue
-      KEYBOARD_KEY_90004=btn_middle
-      KEYBOARD_KEY_90005=key_blue
-      KEYBOARD_KEY_90006=btn_left
-      KEYBOARD_KEY_90007=btn_right
-      KEYBOARD_KEY_90008=key_esc
-  '';
-  fileSystems."/boot".options = [ "umask=0077" ];
-  programs.steam = {
+
+  # DE
+  services.desktopManager.plasma6.enable = true;
+  #services.displayManager.sddm.enable = true; # sddm is hella laggy, I'd prefer using gdm (or something else entirely!)
+  #services.displayManager.sddm.wayland.enable = true;
+  services.xserver = {
     enable = true;
-    #remotePlay.openFirewall = true;
-    #dedicatedServer.openFirewall = true;
-    gamescopeSession.enable = true;
-    package = pkgs.steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        # Needed for gamescope to work
-        # <https://www.reddit.com/r/NixOS/comments/1bmj4mz/gamescope_and_steam/>
-        # <https://github.com/NixOS/nixpkgs/issues/162562#issuecomment-1229444338>
-        libkrb5
-        keyutils
-      ];
+    #desktopManager.gnome.enable = true; # I don't necesserily need gnome and it adds a ton of annoying packages
+    displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = true;
+    videoDrivers = [ "nvidia" ];
+    #videoDrivers = lib.mkForce [ "modesetting" ];
+    xkb.layout = "us,ru";
+  };
+  #programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.ksshaskpass.out}/bin/ksshaskpass"; # https://github.com/NixOS/nixpkgs/issues/75867
+  #hardware.pulseaudio.enable = false; # gnome-only workaround?
+  environment.sessionVariables.NIXOS_OZONE_WL = "1"; # try to run electron apps with ozone so they scale normally
+  environment.sessionVariables.GDK_SCALE = "2"; # try to scale non-native running apps
+  environment.sessionVariables.STEAM_FORCE_DESKTOPUI_SCALING = "1.5"; # force steam to scale
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  # Sound
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
     };
   };
 
-  programs._1password-gui.enable = true;
-  programs.fish.enable = true;
+  programs = {
+    fish.enable = true;
+    _1password-gui.enable = true;
+
+    steam = {
+      enable = true;
+      #remotePlay.openFirewall = true;
+      #dedicatedServer.openFirewall = true;
+      gamescopeSession.enable = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          # Needed for gamescope to work
+          # <https://www.reddit.com/r/NixOS/comments/1bmj4mz/gamescope_and_steam/>
+          # <https://github.com/NixOS/nixpkgs/issues/162562#issuecomment-1229444338>
+          libkrb5
+          keyutils
+        ];
+      };
+    };
+  };
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    htop
+    git
+    micro
+    asusctl
+  ];
+
+
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
@@ -105,11 +156,6 @@
   #   keyMap = "us";
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
   # Enable CUPS to print documents.
   # services.printing.enable = true;
   # Define a user account. Don't forget to set a password with passwd
@@ -182,76 +228,36 @@
       ])
     ];
   };
-  # DE
-  services.desktopManager.plasma6.enable = true;
-  #services.displayManager.sddm.enable = true; # sddm is hella laggy, I'd prefer using gdm (or something else entirely!)
-  #services.displayManager.sddm.wayland.enable = true;
-  services.xserver = {
-    enable = true;
-    #desktopManager.gnome.enable = true; # I don't necesserily need gnome and it adds a ton of annoying packages
-    displayManager.gdm.enable = true;
-    displayManager.gdm.wayland = true;
-    videoDrivers = [ "nvidia" ];
-    #videoDrivers = lib.mkForce [ "modesetting" ];
-    xkb.layout = "us,ru";
-  };
-  programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.ksshaskpass.out}/bin/ksshaskpass"; # https://github.com/NixOS/nixpkgs/issues/75867
-  hardware.pulseaudio.enable = false; # gnome-only workaround?
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  environment.sessionVariables.GDK_SCALE = "2"; # try to scale non-native running apps
-  environment.sessionVariables.STEAM_FORCE_DESKTOPUI_SCALING = "1.5"; # force steam to scale
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-  };
-  hardware = {
-    enableRedistributableFirmware = true;
-    bluetooth.enable = true;
-    nvidia = {
-      modesetting.enable = true;
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-        nvidiaBusId = "PCI:01:0:0";
-        amdgpuBusId = "PCI:69:0:0";
-      };
-    };
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
-  };
-  nixpkgs.config.allowUnfree = true; # we want drivers lol
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    htop
-    git
-    micro
-    asusctl
-  ];
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-  # List services that you want to enable:
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+  services.udev.extraHwdb = ''
+    # Rebinds keys on elecom huge trackball
+    #
+    # tip: You can use `sudo evtest` to get the key values and stuff.
+    #
+    # 90001 = L
+    # 90002 = R
+    # 90003 = M (scroll wheel button)
+    # 90004 = < (back)
+    # 90005 = > (forward)
+    # 90006 = Fn1
+    # 90007 = Fn2
+    # 90008 = Fn3
+    #
+    # key_blue -- noop
+    # btn_0 -- corresponds to key code 256. idk what it does,
+    #          but we can make kwin see it as "scroll" button
+    #          (sadly kwin only wants btn_*, key_* won't work...)
+    evdev:input:b0003v056Ep010D*
+      KEYBOARD_KEY_90001=key_blue
+      KEYBOARD_KEY_90002=btn_0
+      KEYBOARD_KEY_90003=key_blue
+      KEYBOARD_KEY_90004=btn_middle
+      KEYBOARD_KEY_90005=key_blue
+      KEYBOARD_KEY_90006=btn_left
+      KEYBOARD_KEY_90007=btn_right
+      KEYBOARD_KEY_90008=key_esc
+  '';
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
