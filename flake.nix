85723194a62958ef2050ae12e1d691c8b5836dc9
Waffle Lapkin <waffle.lapkin@gmail.com>
2024-05-01 17:10:09 +0200

    Remove chaotic stuff because it was too chaotic for me
    (it was causing kernel crashes on shutdown lmao idk how)

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = inputs@{ nixpkgs, chaotic, ... }: {
+  outputs = inputs@{ nixpkgs, ... }: {
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
     nixosConfigurations.polaris = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix chaotic.nixosModules.default ];
+      modules = [ ./configuration.nix ];
       specialArgs = { inherit inputs; };
     };
   };
