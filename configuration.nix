1bbb122a2c23360ad3d976a7c17286caaf035b2e
Waffle Lapkin <waffle.lapkin@gmail.com>
2024-05-05 03:01:08 +0200

    Enable tailscale

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,7 @@
 
   # List services that you want to enable:
   # Enable the OpenSSH daemon.
+  services.tailscale.enable = true;
   services.openssh.enable = true;
   services.syncthing = {
     enable = true;
