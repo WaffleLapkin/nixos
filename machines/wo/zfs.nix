de76865bfe982d1e501eb04ffe2367b578ef93ce
Waffle Lapkin <waffle.lapkin@gmail.com>
2025-12-26 01:21:30 +0100

    specify kernel compatible with zfs

--- machines/wo/zfs.nix
+++ machines/wo/zfs.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   networking.hostId = "b0224491";
   boot.zfs.extraPools = [ "zpool" ];
   services.zfs.autoScrub.enable = true;
+
+  boot.kernelPackages = lib.mkForce pkgs.zfs.latestCompatibleLinuxPackages;
 }
