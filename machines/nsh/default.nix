4cc0dd0d87c0d012e7ec6b4a25fabdf48a2eeb78
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-01-19 14:12:54 +0100

    enable miri for no significant harassment

--- machines/nsh/default.nix
+++ machines/nsh/default.nix
@@ -16,6 +16,8 @@
     gdm.wayland = true;
   };
 
+  niri.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
