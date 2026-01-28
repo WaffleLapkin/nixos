7fcc390d4e870aa1b003b7a779ef2ce7c635d93a
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-01-28 09:50:47 +0100

    monitor configuration...

--- home/default.nix
+++ home/default.nix
@@ -38,6 +38,33 @@
 
         focus-at-startup = true;
       };
+
+      "Lenovo Group Limited 0x8AB1 Unknown" = {
+        mode = {
+          width = 3072;
+          height = 1920;
+          refresh = 60.0;
+        };
+
+        position = {
+          x = 0;
+          y = 0;
+        };
+
+        scale = 1.5;
+      };
+      "LG Electronics LG ULTRAWIDE 411NTHM28193" = {
+        mode = {
+          width = 3440;
+          height = 1440;
+          refresh = 59.987; # :sob:
+        };
+
+        position = {
+          x = -builtins.floor (3072 / 1.5); # 2048 !
+          y = builtins.floor (1920 / 1.5 - 1440);
+        };
+      };
     };
     debug = { };
     wallpaper = (inputs.self + /media/pink_square.qoi);
