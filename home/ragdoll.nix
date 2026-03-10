c866c4c7d9d9280dad3cb086bd5b098f20142d42
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-03-10 15:20:25 +0100

    move ragdoll shell init stuff + fix r-a not in path issue

--- home/ragdoll.nix
+++ home/ragdoll.nix
@@ -2,4 +2,12 @@
 {
   imports = [ ./. ];
   niri.enable = false;
+
+  programs.fish.interactiveShellInit = ''
+    # without this logging in directly with fish on a non-nixos machine doesn't work :/
+    source /etc/profile.d/nix-daemon.fish
+
+    # that's where pacman puts rust-analyzer proxy: <https://gitlab.archlinux.org/archlinux/packaging/packages/rustup/-/blob/2db544a3e374b13b0001b01dc71d5785649f66ab/PKGBUILD#L54>
+    fish_add_path /usr/lib/rustup/bin
+  '';
 }
