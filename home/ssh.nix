75ed5f6d5a5c78f4177ccafb2d043d264ff1058f
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-03-13 12:18:22 +0100

    set external hostname for ragdoll

--- home/ssh.nix
+++ home/ssh.nix
@@ -12,6 +12,11 @@
         identityAgent = lib.mkIf (!params.external) "~/.1password/agent.sock";
       };
 
+      "ragdoll-ext" = {
+        hostname = "hexcat.nl";
+        port = 4455;
+      };
+
       "ragdoll" = {
         hostname = "192.168.178.138";
       };
