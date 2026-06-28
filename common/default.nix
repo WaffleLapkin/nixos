df4981989c9fb38a29b2de9fd70f68d0e55286e2
Waffle Lapkin <waffle.lapkin@gmail.com>
2026-06-28 23:22:44 +0200

    aseprite is not broken anymore yay

--- common/default.nix
+++ common/default.nix
@@ -206,7 +206,7 @@
     })
     mpv
     wl-clipboard-rs # used by an MPV script I have which allows pasting URLs into mpv
-    # aseprite # broken currently...
+    aseprite
     krita
     inkscape
     # davinci-resolve
